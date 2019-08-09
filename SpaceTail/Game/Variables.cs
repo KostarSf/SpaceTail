3c4090817bf8227d0d88ac920de2260bcefc2c52
kostar <kostarsf@gmail.com>
2019-08-09 03:22:12 +0300

    Create Variables.cs

---
+++ SpaceTail/Game/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTail.Game
+{
+    class Variables
+    {
+        public static string APP_VERSION = "v0.1 ALPHA";
+
+        public static bool DEV_MODE = false;
+    }
+}
