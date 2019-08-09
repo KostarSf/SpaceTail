4df498f5c2bf6c936d737c8e890be7eb5f2ea41a
kostar <kostarsf@gmail.com>
2019-08-09 03:26:52 +0300

    Create GameScene.cs

---
+++ SpaceTail/Game/Scene/GameScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTail.Game.Scenes
+{
+    class GameScene : Scene
+    {
+        public GameScene(string sceneName) : base(sceneName)
+        {
+        }
+    }
+}
