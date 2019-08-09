f271fa34d0f583644f1c3f4f2808673cbfa24e3e
kostar <kostarsf@gmail.com>
2019-08-09 03:22:07 +0300

    Update SpaceTail.cs

--- SpaceTail/SpaceTail.cs
+++ SpaceTail/SpaceTail.cs
@@ -1,143 +1,19 @@
-﻿using System;
+﻿using SpaceTail.Game;
+using System;
 
 namespace SpaceTail
 {
     class SpaceTail
     {
-        public static string appVersion = "v0.1 ALPHA";
-
-        static string[] devMenu_start =
-        {
-            "Welcome to [DarkBlue,]SpaceTail[Reset,]!",
-            "Created by [DarkGray,]KostarSf",
-            "Version " + appVersion,
-            "",
-            " - Menu -",
-            "1. Start game",
-            "2. About",
-            "3. Quit",
-        };
-
-        static string[] devMenu_about =
-        {
-            "[DarkBlue,]SpaceTail[Reset,] " + appVersion,
-            "Created by [DarkGray,]KostarSf",
-            "",
-            "[DarkGray,]A story about one pony",
-            "[DarkGray,]that stuck alone in space.",
-            "",
-            " - Menu -",
-            "1. Github author's page: github.com/KostarSf",
-            "2. Github game's page:   kostarsf.github.io/SpaceTail",
-            "3. Back",
-        };
-
-        public enum Menu
-        {
-            DevMenu,
-            DevMenu_StartGame,
-            DevMenu_About,
-            DevMenu_Quit,
-        }
-
         static void Main(string[] args)
         {
-            Screen.Init();
-            Screen.HideCursor();
-            Screen.Clear();
-            DrawConsoleMenu(Menu.DevMenu);
+            new SpaceTail(args);
         }
 
-        public static void DrawConsoleMenu(Menu menuName)
+        public SpaceTail(string[] args)
         {
-            int chosedMenu;
-
-            switch (menuName)
-            {
-                case Menu.DevMenu:
-                    Console.Clear();
-                    drawTextArray(devMenu_start);
-                    chosedMenu = getIntKeyInput();
-                    switch (chosedMenu)
-                    {
-                        case 1:
-                            DrawConsoleMenu(Menu.DevMenu_StartGame);
-                            break;
-                        case 2:
-                            DrawConsoleMenu(Menu.DevMenu_About);
-                            break;
-                        case 3:
-                            DrawConsoleMenu(Menu.DevMenu_Quit);
-                            break;
-                        default:
-                            DrawConsoleMenu(Menu.DevMenu);
-                            break;
-                    }
-                    break;
-
-                case Menu.DevMenu_StartGame:
-                    Game.StartGame();
-                    break;
-
-                case Menu.DevMenu_About:
-                    Console.Clear();
-                    drawTextArray(devMenu_about);
-                    chosedMenu = getIntKeyInput();
-                    switch (chosedMenu)
-                    {
-                        case 1:
-                            DrawConsoleMenu(Menu.DevMenu_About);
-                            //System.Diagnostics.Process.Start("https://github.com/KostarSf");
-                            break;
-                        case 2:
-                            DrawConsoleMenu(Menu.DevMenu_About);
-                            //System.Diagnostics.Process.Start("https://kostarsf.github.io/SpaceTail");
-                            break;
-                        case 3:
-                            DrawConsoleMenu(Menu.DevMenu);
-                            break;
-                        default:
-                            DrawConsoleMenu(Menu.DevMenu_About);
-                            break;
-                    }
-                    break;
-
-                case Menu.DevMenu_Quit:
-                    Screen.ShowCursor();
-                    Screen.SkipLines(2);
-                    Screen.SimpleText("Farewell! (press any key) ");
-                    Console.ReadKey(true);
-                    Screen.SkipLine();
-                    break;
-
-                default:
-                    DrawConsoleMenu(Menu.DevMenu);
-                    break;
-            }
-        }
-
-        private static int getIntKeyInput()
-        {
-            int inputNumber = 0;
-            int[] inputPos = { Console.CursorLeft, Console.CursorTop };
-
-            Screen.SkipLine();
-            Screen.SimpleText("Your choise is.. ");
-
-            while (Input.GetDigit(ref inputNumber) != true)
-            {
-                Console.SetCursorPosition(inputPos[0], inputPos[1]);
-            }
-
-            return inputNumber;
-        }
-
-        private static void drawTextArray(string[] textArray)
-        {
-            foreach (var textLine in textArray)
-            {
-                Screen.ColoredOutput(textLine + "\n", true);
-            }
+            var manager = new Game.GameManager().SetArgs(args);
+            manager.Start();
         }
     }
 }
